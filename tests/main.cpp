373ca5fdb4517e9f445c430676c8ea81b321e3cf
Azmi ŞAHİN <azmisahin@outlook.com>
2022-05-02 20:21:34 +0000

    ✅ Test: Adding system test

---
+++ tests/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file main.cpp
+ * @author Azmi ŞAHİN (azmisahin@outlook.com)
+ * @brief emulation of risc-based architectures with a cisc architecture.
+ * @version 0.1
+ * @date 2022-05-02
+ *
+ * @copyright Copyright (c) 2022
+ */
+
+/**
+ * Module dependencies
+ */
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+/**
+ * @brief Connector test
+ *
+ * @param argc argument count
+ * @param argv arguments
+ * @return int
+ */
+int main(int argc, char const *argv[])
+{
+    assert(argc > 0);
+
+    cout << "\n💯  All assert passed!" << endl;
+    return 0;
+}
