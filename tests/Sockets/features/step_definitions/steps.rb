41c6f915317c59298f50170dbf028a799f036cbc
Azmi SAHIN <azmisahin@outlook.com>
2022-05-10 13:11:04 +0000

    ✅ : socket steps add bdd test

---
+++ tests/Sockets/features/step_definitions/steps.rb
@@ -0,0 +1,119 @@
+Given(/^I have defined AddressFamiliy\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Unix return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then(/^the return result should be (\d+)$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a InterNetwork return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I have defined the ip address\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I entered the data "([^"]*)" in the ip address\.$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a ToString return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then(/^the return result should be "([^"]*)"$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I have defined the ip endpoint\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I entered the data IpAddres and (\d+) in the ip endpoint\.$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a IpEndPoint return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then(/^the return result should be ipAddres$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I have defined ProtocolType\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Ip return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Icmp return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Igmp return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Ggp return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a IpV(\d+) return\.$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I have defined Socket\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Constractor return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then(/^the return result should be Socket$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Connect return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then(/^the return result should be true$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Send return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Receive return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Id return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I have defined SocketBase\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given(/^I have defined SocketType\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Stream return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When(/^I wanted a Datagram return\.$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
